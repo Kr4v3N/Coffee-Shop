6528dd329668d29771fe7f42ea74a7828d782cdf
Kr4v3n <fafachen37@gmail.com>
2018-09-01 15:20:33 +0200

    Preloader JS

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// ide preloader
+// all the images scripts links have finished loading
+
+// window event list
+window.addEventListener('load', function() {
+    document.querySelector('.preloader').style.display = 'none';
+})
