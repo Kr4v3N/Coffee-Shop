21d7fc587ced523a9790aa0ca7d3caa33746a808
Kr4v3n <fafachen37@gmail.com>
2018-09-01 16:58:32 +0200

    Refactor and Video play/pause

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
-// ide preloader
-// all the images scripts links have finished loading
+// event Listeners
+evenListeners();
 
-// window event list
-window.addEventListener('load', function() {
+function evenListeners() {
+    const ui = new UI()
+    // preloader
+    window.addEventListener('load', function() {
+        ui.hidePreloader();
+    })
+    // nav btn
+    document.querySelector('.navBtn').addEventListener('click', function() {
+        ui.showNav();
+    })
+    // control the video
+    document.querySelector('.video_switch').addEventListener('click', function() {
+        ui.videoControls()
+    })
+}
+
+// constructor function
+function UI() {}
+// hide preloader
+UI.prototype.hidePreloader = function() {
     document.querySelector('.preloader').style.display = 'none';
-})
-// nav btn
-document.querySelector('.navBtn').addEventListener('click', function(){
+}
+// show nav
+UI.prototype.showNav = function() {
     document.querySelector('.nav').classList.toggle('nav--show')
-})
+}
+// play/pause the video
+UI.prototype.videoControls = function() {
+    let btn = document.querySelector('.video_switch-btn');
+    if (!btn.classList.contains('btnSlide')) {
+        btn.classList.add('btnSlide')
+        document.querySelector('.video_item').pause()
+    } else {
+        btn.classList.remove('btnSlide')
+        document.querySelector('.video_item').play()
+    }
+
+}
