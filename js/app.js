6481527009ba3db2127b839ae5a2fedbd6ca1dab
Kr4v3n <fafachen37@gmail.com>
2018-09-01 15:35:02 +0200

    NavbarJS

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@
 window.addEventListener('load', function() {
     document.querySelector('.preloader').style.display = 'none';
 })
+// nav btn
+document.querySelector('.navBtn').addEventListener('click', function(){
+    document.querySelector('.nav').classList.toggle('nav--show')
+})
